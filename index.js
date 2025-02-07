4f04d1bc325d1b4af0cea5862c21f328334a0653
Bharath N <bharath.n208@gmail.com>
2025-02-07 15:52:38 +0530

    features added

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+let posts = [];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+
+app.get("/new", (req,res) => {
+    res.render("new.ejs");
+});
+
+app.post("/new", (req, res) => {
+    const {title, content} = req.body;
+    posts.push({title, content});
+    res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    if (postId >= 0 && postId < posts.length) {
+        res.render("edit.ejs", { post: posts[postId], postId });
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
+
+app.post("/edit/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    if (postId >= 0 && postId < posts.length) {
+        posts[postId] = { id: postId, title: req.body.title, content: req.body.content };
+    }
+    res.redirect("/");
+});
+
+app.get("/delete/:id", (req, res) => {
+    posts.splice(req.params.id, 1);
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
